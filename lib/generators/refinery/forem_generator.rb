789f442d7f0b4163f70a52ddf9704c9227f73408
Sergey <s.bubovich@gmail.com>
2012-03-21 15:12:53 +0300

    Update lib/generators/refinery/forem_generator.rb

--- lib/generators/refinery/forem_generator.rb
+++ lib/generators/refinery/forem_generator.rb
@@ -1,7 +1,7 @@
 require 'refinery/generators'
 
 module Refinery
-  class ForemGenerator < ::Refinery::Generators::EngineInstaller
+  class ForemGenerator < Rails::Generators::Base
 
     source_root File.expand_path('../../../../', __FILE__)
     engine_name "refinerycms-forem"
